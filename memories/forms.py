453825e180ccae50823edbefe7d05a02474197f7
niloofar-samei <niloofar.samei@gmail.com>
2025-08-13 02:40:29 +0100

    Cleanup.

--- memories/forms.py
+++ memories/forms.py
@@ -5,14 +5,7 @@ from .models import Photo
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = ("photo_name",)
+        fields = ("photo",)
+        exclude = ("sender", "published_at")
 
-        labels = {"photo_name": "photo_name"}
-
-        # This block helps to check if the new photo name already exists or not.
-        # In class-based views, we have to use it to check.
-        def clean_photo_name(self):
-            name = self.cleaned_data["photo_name"]
-            if Photo.objects.filter(photo_name__iexact=name).exists():
-                raise form.ValidationError("Sorry but this photo already exists.")
-            return name
+        labels = {"photo": "photo"}
