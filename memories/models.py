268f3cee19da0de37bc6bcddff6a634fa9b5f84a
niloofar-samei <niloofar.samei@gmail.com>
2025-08-13 01:58:30 +0100

    Create model Photo.

--- memories/models.py
+++ memories/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo_name = models.CharField(max_length=200, unique=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.photo_name
