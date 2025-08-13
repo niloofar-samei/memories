48c1a5b735492c2b47858f4ed98ddb158307759c
niloofar-samei <niloofar.samei@gmail.com>
2025-08-13 02:30:00 +0100

    Fix typo.

--- memories/forms.py
+++ memories/forms.py
@@ -2,17 +2,17 @@ from django import forms
 from .models import Photo
 
 
-class PhotoForm(froms.ModelForm):
+class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = "photo_name"
+        fields = ("photo_name",)
 
         labels = {"photo_name": "photo_name"}
 
-        # This block helps to check if the new movie name already exists or not.
+        # This block helps to check if the new photo name already exists or not.
         # In class-based views, we have to use it to check.
-        def clean_movie_name(self):
-            name = self.cleaned_data["movie_name"]
-            if Movie.objects.filter(movie_name__iexact=name).exists():
-                raise form.ValidationError("Sorry but this movie already exists.")
+        def clean_photo_name(self):
+            name = self.cleaned_data["photo_name"]
+            if Photo.objects.filter(photo_name__iexact=name).exists():
+                raise form.ValidationError("Sorry but this photo already exists.")
             return name
