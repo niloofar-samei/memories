c604f29fbfe3fb23b25f215cd2164bc3fcfbb2a9
niloofar-samei <niloofar.samei@gmail.com>
2025-08-13 17:18:32 +0100

    Add hashtag_list and caption to forms.py.

--- memories/forms.py
+++ memories/forms.py
@@ -5,7 +5,11 @@ from .models import Photo
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = ("photo",)
+        fields = ("photo", "caption", "hashtag_list")
         exclude = ("sender", "published_at")
 
-        labels = {"photo": "photo"}
+        labels = {
+            "photo": "photo",
+            "caption": "caption",
+            "hashtag_list": "hashtag list",
+        }
