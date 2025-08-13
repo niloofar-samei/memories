77e94fda8ff857e34581bedf91c295535844a706
niloofar-samei <niloofar.samei@gmail.com>
2025-08-13 02:31:11 +0100

    Create class PhotoCreateView and show the form on the page.

--- memories/urls.py
+++ memories/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from memories.views import IndexListView
+from memories.views import IndexListView, PhotoCreateView
 
 urlpatterns = [
     path("", IndexListView.as_view(), name="IndexListView"),
+    path("new/", PhotoCreateView.as_view(), name="PhotoCreateView"),
 ]
--- memories/views.py
+++ memories/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from .models import Photo
 from django.views.generic import ListView
+from django.shortcuts import render
+from django.views import View
+from memories.forms import PhotoForm
 
 
 class IndexListView(ListView):
     model = Photo
     template_name = "memories/index.html"
     context_object_name = "photos"
+
+
+class PhotoCreateView(View):
+    def get(self, request):
+        return render(request, "memories/new_photo.html", {"photo_form": PhotoForm()})
