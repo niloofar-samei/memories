c353dd4d85defbbb161891476dcc78fe9ab1446e
niloofar-samei <niloofar.samei@gmail.com>
2025-08-13 02:36:22 +0100

    Edit fields.

--- memories/models.py
+++ memories/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Photo(models.Model):
-    photo_name = models.CharField(max_length=200, unique=True)
+    photo = models.ImageField(upload_to="photos/")
+    sender = models.CharField(max_length=100, unique=True)
     published_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.photo_name
+        return self.sender
