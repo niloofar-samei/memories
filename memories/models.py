eb1a91158e61b8cc1c75c07bd73336a188d06248
niloofar-samei <niloofar.samei@gmail.com>
2025-08-14 01:27:44 +0100

    Create Hashtag model.

--- memories/models.py
+++ memories/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 
+class Hashtag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Photo(models.Model):
     photo = models.ImageField(upload_to="photos/")
     sender = models.CharField(max_length=100)
     caption = models.CharField(max_length=500)
-    hashtag_list = models.CharField(max_length=500)
+    hashtags = models.ManyToManyField(Hashtag, related_name="photos")
     published_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.sender
+
+    def get_queryset(self):
+        return Photo.objects.prefetch_related("hashtags")
