687da8b8e9a1b1562bcf12df0e6c731b534ccfd6
niloofar-samei <niloofar.samei@gmail.com>
2025-08-14 02:30:47 +0100

    Save hashtags into database.

--- memories/views.py
+++ memories/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from .models import Photo
+from .models import Photo, Hashtag
 from django.views.generic import ListView
 from django.shortcuts import render, redirect
 from django.views import View
@@ -23,14 +23,19 @@ class PhotoCreateView(View):
             try:
                 photo = request.FILES.get("photo")
                 caption = request.POST.get("caption")
-                hashtags = request.POST.get("hashtags")
+                hashtag_text = request.POST.get("hashtags")
                 new_photo = Photo.objects.create(
                     photo=photo,
                     caption=caption,
-                    hashtag_list=hashtags,
                     sender="niloofar",
                 )
+                hashtag_names = [tag for tag in hashtag_text.split() if tag]
+                for name in hashtag_names:
+                    tag, created = Hashtag.objects.get_or_create(name=name)
+                    new_photo.hashtags.add(tag)
+
                 return redirect("IndexListView")
+
             except Exception as e:
                 print("Error while saving:", e)
                 return redirect("IndexListView")
